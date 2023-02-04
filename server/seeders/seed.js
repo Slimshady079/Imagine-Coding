da031f8067f178b6b4651c2dbbb510eb82d97228
Maximilian Gibes <maximiliangibes@gmail.com>
2023-02-04 07:07:37 -0600

    adds to seed.js in seeders dir. adds seed data to userSeeds.json in seeders dir

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,11 +1,14 @@
 const db = require('../config/connection');
-const { User } = require('../models');
+const { User, Question } = require('../models');
 const userSeeds = require('./userSeeds.json');
+const questionSeeds = require('./questionSeeds.json')
 
 db.once('open', async () => {
   try {
     await User.deleteMany({});
+    await Question.deleteMany({});
     await User.create(userSeeds);
+    await Question.create(questionSeeds);
 
     console.log('all done!');
     process.exit(0);
